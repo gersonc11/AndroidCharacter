c8080f27b0a3846ed1027b162cc99ec6a1811d51
gersonc11 <gcruz1776@gmail.com>
2019-07-08 14:17:02 -0400

    Created 2 pane view for tablet

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -78,15 +78,15 @@ public final class R {
         public static final int gone = 0x7f070043;
         public static final int invisible = 0x7f07004e;
         public static final int left = 0x7f070050;
-        public static final int packed = 0x7f070061;
-        public static final int parent = 0x7f070062;
-        public static final int percent = 0x7f070064;
-        public static final int right = 0x7f070068;
-        public static final int spread = 0x7f070080;
-        public static final int spread_inside = 0x7f070081;
-        public static final int start = 0x7f070086;
-        public static final int top = 0x7f070096;
-        public static final int wrap = 0x7f07009c;
+        public static final int packed = 0x7f070064;
+        public static final int parent = 0x7f070065;
+        public static final int percent = 0x7f070067;
+        public static final int right = 0x7f07006b;
+        public static final int spread = 0x7f070083;
+        public static final int spread_inside = 0x7f070084;
+        public static final int start = 0x7f070089;
+        public static final int top = 0x7f070099;
+        public static final int wrap = 0x7f07009f;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,21 +1,122 @@
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.onItemClickListener {
 
+    int headIndex =0, bodyIndex =0, legIndex =0;
+
+    boolean mTwoPane = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(findViewById(R.id.linear_layout_tablet)!=null) {
+            mTwoPane = true;
+            GridView gridView = findViewById(R.id.master_list_grid_view);
+            gridView.setNumColumns(2);
+
+            findViewById(R.id.nextbtn).setVisibility(View.GONE);
+
+            populateFrameLayouts(savedInstanceState);
+        } else {
+            mTwoPane = false;
+        }
+    }
+
+    private void populateFrameLayouts(Bundle savedInstanceState) {
+        if(savedInstanceState==null) {
+            BodyPartFragment headFragment = new BodyPartFragment();
+
+            headFragment.setmImageIds(AndroidImageAssets.getHeads());
+            headFragment.setmListIndex(1);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction().replace(R.id.head_container, headFragment).commit();
+
+            BodyPartFragment bodyFragment = new BodyPartFragment();
+            bodyFragment.setmImageIds(AndroidImageAssets.getBodies());
+            fragmentManager.beginTransaction().replace(R.id.body_container, bodyFragment).commit();
+
+
+            BodyPartFragment legFragment = new BodyPartFragment();
+            legFragment.setmImageIds(AndroidImageAssets.getLegs());
+            fragmentManager.beginTransaction().replace(R.id.leg_container, legFragment).commit();
+        }
     }
 
     public void onItemClick (int position) {
         Toast.makeText(this, "position clicked " + position, Toast.LENGTH_SHORT).show();
+
+        int indexOfBodyPart = position/12;
+
+        int indexPartClicked = position - 12*indexOfBodyPart;
+
+        if(mTwoPane) {
+            BodyPartFragment bodyPartFragment = new BodyPartFragment();
+            switch (indexOfBodyPart) {
+                case 0:
+                    bodyPartFragment.setmImageIds(AndroidImageAssets.getHeads());
+
+                    bodyPartFragment.setmListIndex(indexPartClicked );
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.head_container, bodyPartFragment).commit();
+                    break;
+                case 1:
+
+                    bodyPartFragment.setmImageIds(AndroidImageAssets.getBodies());
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.body_container, bodyPartFragment).commit();
+
+                    break;
+                case 2:
+
+                    bodyPartFragment.setmImageIds(AndroidImageAssets.getLegs());
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.leg_container, bodyPartFragment).commit();
+
+                    break;
+            }
+
+        } else {
+            switch (indexOfBodyPart) {
+                case 0:
+                    headIndex = indexPartClicked;
+                    break;
+                case 1:
+                    bodyIndex = indexPartClicked;
+                    break;
+                case 2:
+                    legIndex = indexPartClicked;
+                    break;
+            }
+        }
+
+        Bundle b = new Bundle();
+        b.putInt("headIndex", headIndex);
+        b.putInt("bodyIndex", bodyIndex);
+        b.putInt("legIndex", legIndex);
+
+        final Intent i = new Intent(this, AndroidMeActivity.class);
+        i.putExtras(b);
+
+        findViewById(R.id.nextbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(i);
+            }
+        });
     }
 }
